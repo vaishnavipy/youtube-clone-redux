c9c7579cba05879d2bf95743c1dbba637542c53b
vaishpy <ravichandran.vaishu@gmail.com>
2021-08-07 15:42:42 -0500

    added skeleton

--- src/Components/movieCard.js
+++ src/Components/movieCard.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 export default function MovieCard({ videoObj, channel, stat }) {
+    const [loading, setLoading] = useState(true);
     const { title, thumbnails, publishedAt } = videoObj.snippet;
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+    }, []);
 
     return (
         <div
@@ -13,22 +21,31 @@ export default function MovieCard({ videoObj, channel, stat }) {
                 cursor: 'pointer',
             }}
         >
-            <div
-                style={{
-                    height: '70%',
-                    width: '90%',
-                }}
-            >
-                <img
-                    src={thumbnails.medium.url}
-                    alt={videoObj.title}
-                    style={{
-                        height: '100%',
-                        width: '100%',
-                        objectFit: 'contain',
-                    }}
+            {loading ? (
+                <Skeleton
+                    variant="rect"
+                    animation="wave"
+                    width={275}
+                    height={190}
                 />
-            </div>
+            ) : (
+                <div
+                    style={{
+                        height: '70%',
+                        width: '90%',
+                    }}
+                >
+                    <img
+                        src={thumbnails.medium.url}
+                        alt={videoObj.title}
+                        style={{
+                            height: '100%',
+                            width: '100%',
+                            objectFit: 'contain',
+                        }}
+                    />
+                </div>
+            )}
             <div
                 style={{
                     display: 'flex',
@@ -37,59 +54,91 @@ export default function MovieCard({ videoObj, channel, stat }) {
                     height: '30%',
                 }}
             >
-                <div
-                    style={{
-                        borderRadius: '50%',
-                        height: '60%',
-                        width: '25%',
-                    }}
-                >
-                    <img
-                        src={channel && channel.snippet.thumbnails.default.url}
-                        alt={channel && channel.snippet.description}
-                        style={{
-                            width: 'auto',
-                            height: '100%',
-
-                            borderRadius: '50%',
-                        }}
+                {loading ? (
+                    <Skeleton
+                        variant="circle"
+                        animation="wave"
+                        height={50}
+                        width={50}
                     />
-                </div>
+                ) : (
+                    <div
+                        style={{
+                            borderRadius: '50%',
+                            height: '60%',
+                            width: '25%',
+                        }}
+                    >
+                        <img
+                            src={
+                                channel &&
+                                channel.snippet.thumbnails.default.url
+                            }
+                            alt={channel && channel.snippet.description}
+                            style={{
+                                width: 'auto',
+                                height: '100%',
+
+                                borderRadius: '50%',
+                            }}
+                        />
+                    </div>
+                )}
                 <div
                     style={{
                         display: 'flex',
                         flexDirection: 'column',
                     }}
                 >
-                    <p
-                        style={{
-                            margin: '0',
+                    {loading ? (
+                        <Skeleton
+                            variant="text"
+                            animation="wave"
+                            width={220}
+                            height={10}
+                        />
+                    ) : (
+                        <p
+                            style={{
+                                margin: '0',
 
-                            fontSize: '14px',
-                            fontWeight: '900',
-                            color: 'black',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            display: '-webkit-box',
-                            webkitLineClamp: '2',
-                            webkitBoxOrient: 'vertical',
-                        }}
-                    >
-                        {title}
-                    </p>
-                    <p style={{ margin: '0' }}>
-                        {channel && channel.snippet.title}
-                    </p>
-                    {stat && (
-                        <p style={{ margin: '0' }}>
-                            <span style={{ margin: '0' }}>
-                                {stat.statistics.viewCount}
-                            </span>
-                            <span style={{ color: 'black' }}> • </span>
-                            <span>
-                                <Moment fromNow>{publishedAt}</Moment>
-                            </span>
+                                fontSize: '14px',
+                                fontWeight: '900',
+                                color: 'black',
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                display: '-webkit-box',
+                                webkitLineClamp: '2',
+                                webkitBoxOrient: 'vertical',
+                            }}
+                        >
+                            {title}
                         </p>
+                    )}
+                    {loading ? (
+                        <Skeleton
+                            variant="text"
+                            animation="wave"
+                            width={220}
+                            height={10}
+                        />
+                    ) : (
+                        <>
+                            <p style={{ margin: '0' }}>
+                                {channel && channel.snippet.title}
+                            </p>
+                            {stat && (
+                                <p style={{ margin: '0' }}>
+                                    <span style={{ margin: '0' }}>
+                                        {stat.statistics.viewCount}
+                                    </span>
+                                    <span style={{ color: 'black' }}> • </span>
+                                    <span>
+                                        <Moment fromNow>{publishedAt}</Moment>
+                                    </span>
+                                </p>
+                            )}
+                        </>
                     )}
                 </div>
             </div>
